b7481860c99ad3db5ffc33acb2dcc669880df357
Romainrev <romain.revert92@gmail.com>
2018-03-06 15:09:35 +0100

    Création d'une application avec ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from "@angular/router";
+import {FilmModule} from "./film/film.module";
 
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './/app-routing.module';
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FilmModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
